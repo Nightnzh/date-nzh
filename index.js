6b6dc48cf71ae04a004650d82bad61f645ae0449
NightXu <nightxu@NightXudeMac-mini.local>
2022-01-11 02:59:03 +0800

    deliver two

--- index.js
+++ index.js
@@ -8,71 +8,84 @@ $(document).ready(function () {
     loop: true,
     center: true,
     dots: true,
-    autoplayTimeout: 1000,
+    dotsEach: true,
+    // dotsData: true,
+    // autoWidth:true,
+    autoplay: true,
+    autoplayTimeout: 3000,
     autoplayHoverPause: true,
     responsive: {
       0: {
         items: 1
       },
       992: {
+        dots: true,
         items: 3,
         // dots: true,
       },
     }
   });
-  // --
+  // -- Count.js https://inorganik.github.io/countUp.js/
   const c100 = new CountUp('c100', 0, 100);
-  // if (!c100.error) {
-  //   c100.start();
-  // } else {
-  //   console.error(demo.error);
-  // }
+
   const c300 = new CountUp('c300', 0, 300);
-  // if (!c300.error) {
-  //   c300.start();
-  // } else {
-  //   console.error(demo.error);
-  // }
+
   const c93 = new CountUp('c93', 0, 93);
-  // if (!c93.error) {
-  //   c93.start();
-  // } else {
-  //   console.error(demo.error);
-  // }
+
   const c150 = new CountUp('c150', 0, 150);
-  // if (!c150.error) {
-  //   c150.start();
-  // } else {
-  //   console.error(demo.error);
-  // }
+
 
   // -onScroll  
 
+  $(window).on("scroll", () => {
 
-  // ---判斷element 是否可視
-  function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-
-  $(window).on("scroll",()=>{
-    
     // console.log(isInViewport(document.querySelector('#c100')));
-    isInViewport(document.querySelector('#c100')) ? c100.start() : c100.reset()
-    isInViewport(document.querySelector('#c300')) ? c300.start() : c300.reset()
-    isInViewport(document.querySelector('#c93')) ? c93.start() : c93.reset()
-    isInViewport(document.querySelector('#c150')) ? c150.start() : c150.reset()
-    
-  })  
+    $('#c100').visible(true) ? c100.start() : c100.reset()
+    $('#c300').visible(true) ? c300.start() : c300.reset()
+    $('#c93').visible(true) ? c93.start() : c93.reset()
+    $('#c150').visible(true) ? c150.start() : c150.reset()
+
+    if ($('.path').visible(true)) {
+      $(".info-card.i-1").show()
+      $(".info-card.i-2").show()
+      $(".info-card.i-3").show()
+      $(".info-card.i-4").show()
+      $(".info-card.i-5").show()
+      if (!$(".info-card.i-1").hasClass("fade-in-bottom-0s"))
+        $(".info-card.i-1").toggleClass("fade-in-bottom-0s")
+      if (!$(".info-card.i-2").hasClass("fade-in-bottom-03s"))
+        $(".info-card.i-2").toggleClass("fade-in-bottom-03s")
+      if (!$(".info-card.i-3").hasClass("fade-in-bottom-06s"))
+        $(".info-card.i-3").toggleClass("fade-in-bottom-06s")
+      if (!$(".info-card.i-4").hasClass("fade-in-bottom-09s"))
+        $(".info-card.i-4").toggleClass("fade-in-bottom-09s")
+      if (!$(".info-card.i-5").hasClass("fade-in-bottom-12s"))
+        $(".info-card.i-5").toggleClass("fade-in-bottom-12s")
+
+    } else {
+      $(".info-card.i-1").hide()
+      $(".info-card.i-2").hide()
+      $(".info-card.i-3").hide()
+      $(".info-card.i-4").hide()
+      $(".info-card.i-5").hide()
+
+      if ($(".info-card.i-1").hasClass("fade-in-bottom-0s"))
+        $(".info-card.i-1").toggleClass("fade-in-bottom-0s")
+      if ($(".info-card.i-2").hasClass("fade-in-bottom-03s"))
+        $(".info-card.i-2").toggleClass("fade-in-bottom-03s")
+      if ($(".info-card.i-3").hasClass("fade-in-bottom-06s"))
+        $(".info-card.i-3").toggleClass("fade-in-bottom-06s")
+      if ($(".info-card.i-4").hasClass("fade-in-bottom-09s"))
+        $(".info-card.i-4").toggleClass("fade-in-bottom-09s")
+      if ($(".info-card.i-5").hasClass("fade-in-bottom-12s"))
+        $(".info-card.i-5").toggleClass("fade-in-bottom-12s")
+    }
+  })
 
 
-  
+
+  $(".owl-dots").show()
+
   // console.log($(window))
 
 });
